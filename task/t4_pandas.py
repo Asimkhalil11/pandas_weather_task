b6c9fe43cb78e49a994ab66aec9b398605e85028
asim khalil <aximmak11@gmail.com>
2023-04-03 14:24:42 +0500

    var_name_change

--- task/t4_pandas.py
+++ task/t4_pandas.py
@@ -4,8 +4,8 @@ from datetime import datetime
 data = pd.read_csv('/home/asim/Desktop/ISDP/Sir Hammad/pandas_weather_task/files/f2.csv')
 
 events = data[' Events']
-events = data[events.isin(['Thunderstorm'])]
-events_date = pd.to_datetime(events['PKT'])
+thunderstorm_events = data[events.isin(['Thunderstorm'])]
+events_date = pd.to_datetime(thunderstorm_events['PKT'])
 week_day = events_date.dt.strftime('%A').tolist()
 
 print(week_day)
